87cbceecfd1f0e41626fa3ac349c9b7828dce55c
blakebryant00 <blakebryant00@icloud.com>
2025-08-17 21:31:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+function flipTee() {
+  const teeImg = document.getElementById("teeImage");
+  if (teeImg.src.includes("prod-tee-front.png")) {
+    teeImg.src = "prod-tee-back.png";
+  } else {
+    teeImg.src = "prod-tee-front.png";
+  }
+}
